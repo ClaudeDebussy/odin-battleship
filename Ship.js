e1ec006d238844cc68a602d49c2d9e3d2928a955
ClaudeDebussy <15807600+ClaudeDebussy@users.noreply.github.com>
2025-11-13 16:03:30 +0500

    implement Ship class and tests

--- Ship.js
+++ Ship.js
@@ -1,1 +1,19 @@
-export class Ship {}
+export class Ship {
+  constructor(length) {
+    this.length = length
+    this.timesHit = 0
+    this.sunk = false
+  }
+
+  hit() {
+    this.timesHit++
+    // check if is sunk
+    this.isSunk()
+  }
+
+  isSunk() {
+    if (this.timesHit === this.length) {this.sunk = true} 
+  }
+
+
+}
